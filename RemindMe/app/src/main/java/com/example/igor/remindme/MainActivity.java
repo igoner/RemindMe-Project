7ccc7d8426cc4b45a650a68bffd7e5977275dcfb
igor <starcraft450@gmail.com>
2017-07-30 10:50:15 +0500

    Create ActivityMain  class and layout.

---
+++ RemindMe/app/src/main/java/com/example/igor/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.igor.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by igor on 30.07.17.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
